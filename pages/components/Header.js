63a93aa373ec90b89fb70545359bf1c115b2cfc7
y.bigdeli <yousefbigdeli96@gmail.com>
2020-04-13 18:03:17 +0430

    create styles and components directory

---
+++ pages/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Header.module.scss';
+
+const Header = () => {
+    return (
+        <>
+            <div className={styles.header}></div>
+        </>
+    );
+}
+
+export default Header;
