beb955520cf4554b2315129fbbee00c22bab6fbc
y.bigdeli <yousefbigdeli96@gmail.com>
2020-04-27 23:04:28 +0430

    add slider to index page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Template from './components/Template';
-
+import Slider from './components/Slider';
 
 //home page on site
 function Index() {
   return (
       <>
         <Template>
-            
+            {/* Slider component*/}
+            <Slider />
+            {/*  */}
         </Template>
       </>
   );
