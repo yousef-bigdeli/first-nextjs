e5d23fe40e98baa31206f8ea1a05eec7f9550aa6
y.bigdeli <yousefbigdeli96@gmail.com>
2020-06-10 23:02:26 +0430

    undoing changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Template from './components/Template';
 import Slider from './components/Slider';
+import styles from './styles/Index.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight,  faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 
 //home page on site
 function Index() {
