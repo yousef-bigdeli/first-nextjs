7f87b21983f3c86ed662391e62acedb485cf054a
yousef-bigdeli <yousefbigdeli96@gmail.com>
2020-07-31 16:43:45 +0430

    creating the register page

---
+++ pages/register.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Template from './components/Template.js';
+
+function Register() {
+    return (
+        <>
+            <Template>
+                <h1>Hello</h1>
+            </Template>
+        </>
+    )
+}
+
+export default Register;
